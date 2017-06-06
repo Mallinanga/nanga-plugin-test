2bc2716703bc5c1c9bcd2b83b6f2ce80ad63eea9
Panos Paganis <mallinanga@gmail.com>
2017-06-06 16:11:13 +0300

    Update nanga-plugin-test.php

--- nanga-plugin-test.php
+++ nanga-plugin-test.php
@@ -1,1 +1,10 @@
 
+<?php
+/*
+Plugin Name: VG web things Plugin - Testing
+Description: This is just a placeholder.
+Author: VG web things
+Version: 1.0.0
+Text Domain: vg
+Domain Path: /languages/
+*/
