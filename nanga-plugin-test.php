8d5925bb3cc2087213e055a2e760026f649e1e49
Panos Paganis <mallinanga@gmail.com>
2017-06-18 19:38:47 +0300

    WIP

--- nanga-plugin-test.php
+++ nanga-plugin-test.php
@@ -1,9 +1,7 @@
 <?php
 /*
-Plugin Name: VG web things Plugin - Placeholder
-Description: This is just a placeholder.
 Author: VG web things
-Version: 1.0.1
-Text Domain: vg
-Domain Path: /languages/
+Description: This is just a placeholder.
+Plugin Name: VG web things Plugin - Placeholder
+Version: 1.0.2
 */
