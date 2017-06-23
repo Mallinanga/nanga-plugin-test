57e5a6356f9a1e8690d61b17ef160ee27dbb6f92
Panos Paganis <mallinanga@gmail.com>
2017-06-23 10:39:15 +0300

    WIP

--- nanga-plugin-test.php
+++ nanga-plugin-test.php
@@ -1,7 +1,8 @@
 <?php
 /*
-Author: VG web things
+Author URI: https://github.com/Mallinanga/
+Author: Panos Paganis
 Description: This is just a placeholder.
 Plugin Name: VG web things Plugin - Placeholder
-Version: 1.0.2
+Version: 1.0.3
 */
